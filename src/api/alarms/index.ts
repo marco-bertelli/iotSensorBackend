df7012d6e7ec759992b8b15a2fb44a8b3bccc77b
Marco <marcobert37@gmail.com>
2022-08-27 15:09:58 +0200

    feat(alarams): finish alarms call + redis cache

--- src/api/alarms/index.ts
+++ src/api/alarms/index.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
 import { Alarm } from '../../models/alarms';
+import { redis } from '../../services/redis';
 
 const router = new (Router as any)();
 
 router.get('/active', async (req: any, res: { send: (arg0: any) => void; }) => {
     const activeAlarms = await Alarm.find({ type: 'active' });
     res.send(activeAlarms)
+});
+
+router.get('/finished', async (req: any, res: { send: (arg0: any) => void; }) => {
+    let result: any = await redis.get('finishedAlarms');
+
+    // caching logic
+    if (!result) {
+        result = await Alarm.find({ type: 'finish' });
+
+        // 3 minutes cache for this call
+        await redis.set('finishedAlarms', JSON.stringify(result), {
+            EX: 180,
+            NX: true,
+        })
+    }
+    res.send(JSON.parse(result))
 });
 
 export default router;

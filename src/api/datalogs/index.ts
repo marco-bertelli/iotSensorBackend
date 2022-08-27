548a27e57e7591ccdc854e1c4766f0082062fb1e
Marco <marcobert37@gmail.com>
2022-08-27 16:17:14 +0200

    feat(predictions)

--- src/api/datalogs/index.ts
+++ src/api/datalogs/index.ts
@@ -3,9 +3,26 @@ import { dataLogRepository } from '../../services/redis';
 import { actualDataAggregation, humidityAverageAggregate, temperatureAverageAggregate } from './aggregations';
 import { DataLog } from '../../models/datalogs';
 
+const tf = require('@tensorflow/tfjs-node')
+
 import moment from 'moment';
 
 const router = new (Router as any)();
+
+router.get('/predict', async ({ params: { sensorId } }: any, res: { send: (arg0: any) => void; }) => {
+    const nextHour = moment().add(1, 'hours').unix()
+    const humidity = Number(80);
+
+    const handler = tf.io.fileSystem(__dirname + "/../../tensorflow/model.json");
+    const model = await tf.loadLayersModel(handler);
+
+    const tensor = tf.tensor([[Number(nextHour), Number(humidity)]])
+
+    const prediction = await model.predict(tensor)
+    const values = prediction.dataSync();
+
+    res.send(values)
+});
 
 router.get('/ambient/now', async (req: any, res: { send: (arg0: any) => void; }) => {
     const start = moment().startOf('day').unix()
